d2dae5ebc4b89d94127f034d0f5fedb2fa666a91
Huy Phạm <pqhuy87it@gmail.com>
2020-08-07 10:01:23 +0900

    update.

--- Swift-Extension/Swift-Extension/Extensions/String+Extension.swift
+++ Swift-Extension/Swift-Extension/Extensions/String+Extension.swift
@@ -129,4 +129,40 @@ extension String {
         
         return nil
     }
+
+	// MARK: - Hashtag
+
+	func hashtags() -> [String]?
+	{
+		if let regex = try? NSRegularExpression(pattern: "#[a-z0-9]+", options: .caseInsensitive)
+		{
+			let string = self as NSString
+
+			return regex.matches(in: self, options: [], range: NSRange(location: 0, length: string.length)).map {
+				string.substring(with: $0.range).replacingOccurrences(of: "#", with: "").lowercased()
+			}
+		}
+
+		return nil
+	}
+
+	// MARK: - Prefix Suffix
+
+	// Prefix
+
+	func caseInsensitiveHasPrefix(_ prefix: String) -> Bool {
+		return lowercased().hasPrefix(prefix.lowercased())
+	}
+
+	// Suffix
+
+	func caseInsensitiveHasSuffix(_ suffix: String) -> Bool {
+		return lowercased().hasSuffix(suffix.lowercased())
+	}
+
+	// NSLocalizedString
+
+	var localized: String {
+		return NSLocalizedString(self, comment: "")
+	}
 }

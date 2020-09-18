f4fb6211898ff32903718574829ad2fad8c938cc
Pham Quang Huy <pqhuy87it@gmail.com>
2020-09-18 23:16:53 +0900

    update string.

--- Swift-Extension/Swift-Extension/Extensions/StringProtocol+Extension.swift
+++ Swift-Extension/Swift-Extension/Extensions/StringProtocol+Extension.swift
@@ -38,4 +38,22 @@ extension StringProtocol {
 	var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
 	
 	var firstCapitalized: String { prefix(1).capitalized + dropFirst() }
+    
+    subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
+    
+    subscript(range: Range<Int>) -> SubSequence {
+        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+    }
+    
+    subscript(range: ClosedRange<Int>) -> SubSequence {
+        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+    }
+    
+    subscript(range: PartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
+    
+    subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
+    
+    subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
 }
